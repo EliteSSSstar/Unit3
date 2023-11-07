6c56111fb2aeb208442fa640176a29452ae76c16
EliteSSSstar <B00140975@mytudublin.ie>
2023-11-07 14:16:41 +0000

    ipdate
    updaty2

---
+++ challenge 3/Assets/Challenge 3/Scripts/Link.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void OpenLinkJSPlugin()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://elitesssstar.github.io/Y2-3-Repositery/");
+		//Application.OpenUrl("https://elitesssstar.github.io/Y2-3-Repositery/");
+
+		#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
